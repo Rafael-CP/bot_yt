bf252e0519fed0f9059412e9bdfd1984e6b21f9d
rafael <rafaelcpaes@unifei.edu.br>
2021-03-31 03:13:18 -0300

    c1 - passando link parametrizado e dando play

--- bot_YT/bot_YT/Program.cs
+++ bot_YT/bot_YT/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 
 namespace bot_YT
 {
@@ -6,7 +10,19 @@ namespace bot_YT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["link"]);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                driver.FindElement(By.XPath("//*[@id='movie_player']/div[4]/button")).Click();
+              
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());                
+            }
+
         }
     }
 }
